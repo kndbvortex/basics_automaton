b03175c74e88daf542cdf2502256037118023830
kndb_vortex_TTCY <kamganguifo@gmail.com>
2021-06-03 22:18:28 +0100

    i exit
    git commit -m i

---
+++ Test_node.py
@@ -0,0 +1,38 @@
+class node:
+    def __init__(self, n) -> None:
+        self.info = n
+        self.suivant = None
+        self.parent = None
+
+    def __str__(self) -> str:
+        return f'{self.info} ----> '
+
+    def add(self, x):
+        a = self
+        q = a
+        while a.suivant != None:
+            a = a.suivant
+        a.suivant = node(x) 
+        a.suivant.parent = a
+
+    def print(self):
+        a = self
+        while a != None:
+            print(a)
+            a = a.suivant
+
+    def test(self):
+        a = node(1)
+        for i in range(3,7):
+            print(i)
+            a.add(i)
+        a.print()
+
+if __name__ == '__main__':
+    from sys import getsizeof
+    a = node(1)
+    print(getsizeof(a))
+    for i in range(3,7):
+        a.add(i)
+    print(getsizeof(a))
+    #a.print()
